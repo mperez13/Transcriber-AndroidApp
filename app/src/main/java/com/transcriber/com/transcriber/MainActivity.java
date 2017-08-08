b8e3a23f22a57581bf2fc46f2edf8eff2a5502df
OnezEgo <slavetoego@gmail.com>
2017-08-07 23:04:13 -0700

    audio recording

--- app/src/main/java/com/transcriber/com/transcriber/MainActivity.java
+++ app/src/main/java/com/transcriber/com/transcriber/MainActivity.java
@@ -1,12 +1,14 @@
 package com.transcriber.com.transcriber;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int ACTIVITY_RECORD_SOUND = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
             case R.id.tts:
                 Intent j = new Intent(getApplicationContext(), TextToSpeech.class);
                 startActivity(j);
+                return true;
+            case R.id.record:
+                Intent k = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+                startActivityForResult(k, ACTIVITY_RECORD_SOUND);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
